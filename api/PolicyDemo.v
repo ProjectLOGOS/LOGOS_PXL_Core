d00687f7c4eca11b16a6d73de2bbf247d5761aa9
ChronoPraxis IEL <chronopraxis@logos.dev>
2025-10-09 01:50:15 -0400

    Windows-safe Coq build; tighten hygiene regex; minimal PolicyDemo

--- api/PolicyDemo.v
+++ api/PolicyDemo.v
@@ -1,7 +1,5 @@
-From PXLs.IEL.Pillars.ThemiPraxis Require Import Core.
-From PXLs.IEL.Pillars.GnosiPraxis Require Import Core.
 From PXLs.API Require Import ProofToken ToolGate.
 Module PolicyDemo.
-  Definition Policy := True.                 (* replace with Oφ ∧ Kφ form *)
+  Definition Policy := True.
   Definition run (t:ProofToken.Token Policy) := ToolGate.allow _ t.
 End PolicyDemo.
