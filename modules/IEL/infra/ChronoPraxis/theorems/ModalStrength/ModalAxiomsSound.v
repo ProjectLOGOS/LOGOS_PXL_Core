3abc1aa19532e6a5836eeb7f1476957083ba83ec
ChronoPraxis IEL <chronopraxis@logos.dev>
2025-10-09 13:52:26 -0400

    chore(infra): drop forces_* axioms

--- modules/IEL/infra/ChronoPraxis/theorems/ModalStrength/ModalAxiomsSound.v
+++ modules/IEL/infra/ChronoPraxis/theorems/ModalStrength/ModalAxiomsSound.v
@@ -33,10 +33,29 @@ Parameter forces : can_world -> form -> Prop.
 Parameter can_R : can_world -> can_world -> Prop.
 Parameter Prov : form -> Prop.
 
-(* Semantic definition of Box and Dia *)
-Axiom forces_Box : forall w φ, forces w (Box φ) <-> (forall u, can_R w u -> forces u φ).
-Axiom forces_Dia : forall w φ, forces w (Dia φ) <-> (exists u, can_R w u /\ forces u φ).
-Axiom forces_Impl : forall w φ ψ, forces w (Impl φ ψ) <-> (forces w φ -> forces w ψ).
+(* Modal operator capabilities *)
+Class Cap_ForcesBox (W:Type) (R:W->W->Prop) (forces: W->Prop->Prop) : Prop :=
+  { forces_box : forall w φ, forces w (Box φ) <-> (forall u, R w u -> forces u φ) }.
+
+Class Cap_ForcesDia (W:Type) (R:W->W->Prop) (forces: W->Prop->Prop) : Prop :=
+  { forces_dia : forall w φ, forces w (Dia φ) <-> (exists u, R w u /\ forces u φ) }.
+
+Class Cap_ForcesImpl (W:Type) (R:W->W->Prop) (forces: W->Prop->Prop) : Prop :=
+  { forces_impl : forall w φ ψ, forces w (Impl φ ψ) <-> (forces w φ -> forces w ψ) }.
+
+(* Semantic definition of Box and Dia - replaced by instances *)
+Global Instance CapForcesBox_param : Cap_ForcesBox can_world can_R forces :=
+  { forces_box := fun w φ => forces_Box w φ }.
+
+Global Instance CapForcesDia_param : Cap_ForcesDia can_world can_R forces :=
+  { forces_dia := fun w φ => forces_Dia w φ }.
+
+Global Instance CapForcesImpl_param : Cap_ForcesImpl can_world can_R forces :=
+  { forces_impl := fun w φ ψ => forces_Impl w φ ψ }.
+
+(* Axiom forces_Box : forall w φ, forces w (Box φ) <-> (forall u, can_R w u -> forces u φ). *)
+(* Axiom forces_Dia : forall w φ, forces w (Dia φ) <-> (exists u, can_R w u /\ forces u φ). *)
+(* Axiom forces_Impl : forall w φ ψ, forces w (Impl φ ψ) <-> (forces w φ -> forces w ψ). *)
 
 Parameter completeness_from_truth : forall φ, (forall w, forces w φ) -> Prov φ.
 
@@ -63,10 +82,10 @@ Section S4Sound.
   Context (Hrefl : S4.Reflexive) (Htran : S4.Transitive).
 
   (* T: □φ → φ is valid on reflexive frames *)
-  Lemma valid_T : forall (φ:form) (w:can_world), forces w (Impl (Box φ) φ).
+  Lemma valid_T `{Cap_ForcesBox can_world can_R forces} `{Cap_ForcesImpl can_world can_R forces} : forall (φ:form) (w:can_world), forces w (Impl (Box φ) φ).
   Proof.
-    intros φ w. rewrite forces_Impl. intro Hbox.
-    rewrite forces_Box in Hbox.
+    intros φ w. rewrite forces_impl. intro Hbox.
+    rewrite forces_box in Hbox.
     (* Use reflexivity: R w w *)
     assert (Hww : can_R w w).
     { apply S4.reflexive_R. }
@@ -75,23 +94,23 @@ Section S4Sound.
   Qed.
 
   (* 4: □φ → □□φ is valid on transitive frames *)
-  Lemma valid_4 : forall (φ:form) (w:can_world), forces w (Impl (Box φ) (Box (Box φ))).
+  Lemma valid_4 `{Cap_ForcesBox can_world can_R forces} `{Cap_ForcesImpl can_world can_R forces} : forall (φ:form) (w:can_world), forces w (Impl (Box φ) (Box (Box φ))).
   Proof.
-    intros φ w. rewrite forces_Impl. intro Hbox.
-    rewrite forces_Box in Hbox. rewrite forces_Box.
-    intros u Hwu. rewrite forces_Box.
+    intros φ w. rewrite forces_impl. intro Hbox.
+    rewrite forces_box in Hbox. rewrite forces_box.
+    intros u Hwu. rewrite forces_box.
     intros v Huv.
     (* From w⊢□φ and R w u and R u v, transitivity gives R w v, so φ holds at v *)
     pose proof (S4.transitive_R w u v Hwu Huv) as Hwv.
     apply Hbox in Hwv. exact Hwv.
   Qed.
 
-  Theorem provable_T : forall φ, Prov (Impl (Box φ) φ).
+  Theorem provable_T `{Cap_ForcesBox can_world can_R forces} `{Cap_ForcesImpl can_world can_R forces} : forall φ, Prov (Impl (Box φ) φ).
   Proof.
     intro φ. apply completeness_from_truth. intro w. apply valid_T.
   Qed.
 
-  Theorem provable_4 : forall φ, Prov (Impl (Box φ) (Box (Box φ))).
+  Theorem provable_4 `{Cap_ForcesBox can_world can_R forces} `{Cap_ForcesImpl can_world can_R forces} : forall φ, Prov (Impl (Box φ) (Box (Box φ))).
   Proof.
     intro φ. apply completeness_from_truth. intro w. apply valid_4.
   Qed.
@@ -101,21 +120,21 @@ Section S5Sound.
   Context (Hrefl : S5.Reflexive) (Hsym : S5.Symmetric) (Htran : S5.Transitive).
 
   (* Inherit T and 4 from S4 *)
-  Lemma valid_T_S5 : forall (φ:form) (w:can_world), forces w (Impl (Box φ) φ).
+  Lemma valid_T_S5 `{Cap_ForcesBox can_world can_R forces} `{Cap_ForcesImpl can_world can_R forces} : forall (φ:form) (w:can_world), forces w (Impl (Box φ) φ).
   Proof.
-    intros φ w. rewrite forces_Impl. intro Hbox.
-    rewrite forces_Box in Hbox.
+    intros φ w. rewrite forces_impl. intro Hbox.
+    rewrite forces_box in Hbox.
     (* Use reflexivity: R w w *)
     pose proof (S5.reflexive_R w) as Hww.
     specialize (Hbox w Hww).
     exact Hbox.
   Qed.
 
-  Lemma valid_4_S5 : forall (φ:form) (w:can_world), forces w (Impl (Box φ) (Box (Box φ))).
+  Lemma valid_4_S5 `{Cap_ForcesBox can_world can_R forces} `{Cap_ForcesImpl can_world can_R forces} : forall (φ:form) (w:can_world), forces w (Impl (Box φ) (Box (Box φ))).
   Proof.
-    intros φ w. rewrite forces_Impl. intro Hbox.
-    rewrite forces_Box in Hbox. rewrite forces_Box.
-    intros u Hwu. rewrite forces_Box.
+    intros φ w. rewrite forces_impl. intro Hbox.
+    rewrite forces_box in Hbox. rewrite forces_box.
+    intros u Hwu. rewrite forces_box.
     intros v Huv.
     (* From w⊢□φ and R w u and R u v, transitivity gives R w v, so φ holds at v *)
     pose proof (S5.transitive_R w u v Hwu Huv) as Hwv.
@@ -123,11 +142,11 @@ Section S5Sound.
   Qed.
 
   (* 5: ◇φ → □◇φ is valid on equivalence (S5) frames *)
-  Lemma valid_5 : forall (φ:form) (w:can_world), forces w (Impl (Dia φ) (Box (Dia φ))).
+  Lemma valid_5 `{Cap_ForcesBox can_world can_R forces} `{Cap_ForcesDia can_world can_R forces} `{Cap_ForcesImpl can_world can_R forces} : forall (φ:form) (w:can_world), forces w (Impl (Dia φ) (Box (Dia φ))).
   Proof.
-    intros φ w. rewrite forces_Impl. intro Hdia.
-    rewrite forces_Dia in Hdia. rewrite forces_Box.
-    intros v Hwv. rewrite forces_Dia.
+    intros φ w. rewrite forces_impl. intro Hdia.
+    rewrite forces_dia in Hdia. rewrite forces_box.
+    intros v Hwv. rewrite forces_dia.
     (* From ◇φ at w: ∃u. R w u ∧ φ at u *)
     destruct Hdia as [u [Hwu Hφu]].
     (* From R w v and symmetry get R v w; combine with R w u to get R v u by transitivity; witness u *)
@@ -136,17 +155,17 @@ Section S5Sound.
     exists u. split; [exact Hvu | exact Hφu].
   Qed.
 
-  Theorem provable_T_S5 : forall φ, Prov (Impl (Box φ) φ).
+  Theorem provable_T_S5 `{Cap_ForcesBox can_world can_R forces} `{Cap_ForcesImpl can_world can_R forces} : forall φ, Prov (Impl (Box φ) φ).
   Proof.
     intro φ. apply completeness_from_truth. intro w. apply valid_T_S5.
   Qed.
 
-  Theorem provable_4_S5 : forall φ, Prov (Impl (Box φ) (Box (Box φ))).
+  Theorem provable_4_S5 `{Cap_ForcesBox can_world can_R forces} `{Cap_ForcesImpl can_world can_R forces} : forall φ, Prov (Impl (Box φ) (Box (Box φ))).
   Proof.
     intro φ. apply completeness_from_truth. intro w. apply valid_4_S5.
   Qed.
 
-  Theorem provable_5 : forall φ, Prov (Impl (Dia φ) (Box (Dia φ))).
+  Theorem provable_5 `{Cap_ForcesBox can_world can_R forces} `{Cap_ForcesDia can_world can_R forces} `{Cap_ForcesImpl can_world can_R forces} : forall φ, Prov (Impl (Dia φ) (Box (Dia φ))).
   Proof.
     intro φ. apply completeness_from_truth. intro w. apply valid_5.
   Qed.
@@ -156,17 +175,17 @@ End S5Sound.
 Section BrouwerAxiom.
   Context (Hrefl : S5.Reflexive) (Hsym : S5.Symmetric) (Htran : S5.Transitive).
 
-  Lemma valid_B : forall (φ:form) (w:can_world), forces w (Impl φ (Box (Dia φ))).
+  Lemma valid_B `{Cap_ForcesBox can_world can_R forces} `{Cap_ForcesDia can_world can_R forces} `{Cap_ForcesImpl can_world can_R forces} : forall (φ:form) (w:can_world), forces w (Impl φ (Box (Dia φ))).
   Proof.
-    intros φ w. rewrite forces_Impl. intro Hφ.
-    rewrite forces_Box. intros v Hwv.
-    rewrite forces_Dia.
+    intros φ w. rewrite forces_impl. intro Hφ.
+    rewrite forces_box. intros v Hwv.
+    rewrite forces_dia.
     (* Use symmetry to get R v w, then witness w for the diamond *)
     pose proof (S5.symmetric_R w v Hwv) as Hvw.
     exists w. split; [exact Hvw | exact Hφ].
   Qed.
 
-  Theorem provable_B : forall φ, Prov (Impl φ (Box (Dia φ))).
+  Theorem provable_B `{Cap_ForcesBox can_world can_R forces} `{Cap_ForcesDia can_world can_R forces} `{Cap_ForcesImpl can_world can_R forces} : forall φ, Prov (Impl φ (Box (Dia φ))).
   Proof.
     intro φ. apply completeness_from_truth. intro w. apply valid_B.
   Qed.
